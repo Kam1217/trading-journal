ea57304e836a4f45db260fc93a7e0c58d4c91906
Kam1217 <kamilaostroga@gmail.com>
2025-07-03 16:40:06 +0100

    Upload and save csv file to folder function

---
+++ trading_portfolio/pnl_calendar/handle_csv_upload.py
@@ -0,0 +1,6 @@
+#Upload and save csv file to a folder
+
+def handle_upload_csv(f):
+    with open("pnl_calendar/trades_csv"+f.name,"wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
