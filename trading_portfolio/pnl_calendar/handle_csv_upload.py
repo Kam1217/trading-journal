5772e787b59aa2a617561217a12c0390ab2014a9
Kam1217 <kamilaostroga@gmail.com>
2025-07-03 20:36:49 +0100

    Upload and save required data to the Trades model from csv

--- trading_portfolio/pnl_calendar/handle_csv_upload.py
+++ trading_portfolio/pnl_calendar/handle_csv_upload.py
@@ -1,7 +1,10 @@
+import csv
+from datetime import datetime
 import uuid
 import os
-#Upload and save csv file to a folder
+from .models import Trades
 
+#Upload and save csv file to a folder
 def handle_upload_csv(f):
     #Make a unique name for each file about to me uploaded
     unique_csv_id = uuid.uuid4().hex 
@@ -12,3 +15,22 @@ def handle_upload_csv(f):
     with open(f"pnl_calendar/trades_csv/{unique_csv_name}", "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    
+    #Get required csv data for the Trades model
+    desired_keys = {"Date/Time": "trade_date", "Gross P/L": "gross_pnl", "Fee": "fee", "Net P/L": "net_pnl", "Trade ID": "trade_id"}
+
+    with open(f"pnl_calendar/trades_csv/{unique_csv_name}", mode= "r") as file:
+        csv_file = csv.DictReader(file)
+
+        #Filter each row to desired headers
+        for row in csv_file:
+            filtered_row = {desired_keys[key] for key in desired_keys.keys() if key in row}
+
+            #Convert date to correct model format for Trades model
+            date_obj = datetime.strptime(filtered_row["trade_date"], "%d/%m/%Y %H:%M:%S %z")
+            filtered_row["trade_date"] = date_obj.strftime("%Y-%m-%d %H:%M:%S%z")
+
+            #Save to data to Trades model
+            trades = Trades(**filtered_row)
+            trades.save()
+
