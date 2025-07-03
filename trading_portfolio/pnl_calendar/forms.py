40911d1f670ec64e61474db936afda2c443f93c1
Kam1217 <kamilaostroga@gmail.com>
2025-07-03 19:11:32 +0100

    delete prints for debugging

--- trading_portfolio/pnl_calendar/forms.py
+++ trading_portfolio/pnl_calendar/forms.py
@@ -10,16 +10,8 @@ class UploadForm(forms.Form):
     #Function that checks if it is a csv file - if not throw an error
     def clean_csv_file(self):
         file = self.cleaned_data.get("csv_file")  
-        print(f"File value: {repr(file)}")  # This will show exactly what file is
-        print(f"File type: {type(file)}")
-        print(f"File is None: {file is None}")
-        print(f"File is falsy: {not file}")
-        
         if not file:
-            print("No file - returning early")
             return file
-        
-        print("File exists - validating...")
         file_extension = os.path.splitext(file.name)[1].lower()
         if file_extension != '.csv':
             raise ValidationError("File must be a CSV file")
