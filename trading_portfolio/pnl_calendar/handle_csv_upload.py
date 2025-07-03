f6764d6fd2b91bcb3a8916001379b1058d9abc8b
Kam1217 <kamilaostroga@gmail.com>
2025-07-03 21:21:04 +0100

    Remove unique name for now

--- trading_portfolio/pnl_calendar/handle_csv_upload.py
+++ trading_portfolio/pnl_calendar/handle_csv_upload.py
@@ -7,19 +7,19 @@ from .models import Trades
 #Upload and save csv file to a folder
 def handle_upload_csv(f):
     #Make a unique name for each file about to me uploaded
-    unique_csv_id = uuid.uuid4().hex 
-    name, ext = os.path.splitext(f.name)
-    unique_csv_name = f"{unique_csv_id}_{name}{ext}"
+    # unique_csv_id = uuid.uuid4().hex 
+    # name, ext = os.path.splitext(f.name)
+    # unique_csv_name = f"{unique_csv_id}_{name}{ext}"
     
     #Open and save the uploaded csv file to trades_csv folder   
-    with open(f"pnl_calendar/trades_csv/{unique_csv_name}", "wb+") as destination:
+    with open(f"pnl_calendar/trades_csv/"+f.name, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
     
     #Get required csv data for the Trades model
     desired_keys = {"Date/Time": "trade_date", "Gross P/L": "gross_pnl", "Fee": "fee", "Net P/L": "net_pnl", "Trade ID": "trade_id"}
 
-    with open(f"pnl_calendar/trades_csv/{unique_csv_name}", mode= "r") as file:
+    with open(f"pnl_calendar/trades_csv/"+f.name, mode= "r") as file:
         csv_file = csv.DictReader(file)
 
         #Filter each row to desired headers
