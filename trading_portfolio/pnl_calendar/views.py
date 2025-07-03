b772ee8100d1d8d3ee4d65fd057911545e093b2f
Kam1217 <kamilaostroga@gmail.com>
2025-07-03 16:53:15 +0100

    Upload and save form

--- trading_portfolio/pnl_calendar/views.py
+++ trading_portfolio/pnl_calendar/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .forms import UploadForm
+from .handle_csv_upload import handle_upload_csv
 
 # Create your views here.
 
 def pnl_calendar(request):
     context = {}
-    context['form']= UploadForm()
-    return render(request, "pnl_calendar.html", context)
+    if request.POST:
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid:
+            handle_upload_csv(request.FILES["csv_file"])
+    else:
+        form = UploadForm()
+    context["form"] = form
+    return render(request,"pnl_calendar.html", context)
 
