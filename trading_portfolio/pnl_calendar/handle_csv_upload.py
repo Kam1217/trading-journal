c8ca916e962d4051e9cfcf656c48930cfc0592d9
Kam1217 <kamilaostroga@gmail.com>
2025-07-03 20:09:11 +0100

    Add a unique name to each csv as its being uploaded

--- trading_portfolio/pnl_calendar/handle_csv_upload.py
+++ trading_portfolio/pnl_calendar/handle_csv_upload.py
@@ -1,6 +1,14 @@
+import uuid
+import os
 #Upload and save csv file to a folder
 
 def handle_upload_csv(f):
-    with open("pnl_calendar/trades_csv/"+f.name,"wb+") as destination:
+    #Make a unique name for each file about to me uploaded
+    unique_csv_id = uuid.uuid4().hex 
+    name, ext = os.path.splitext(f.name)
+    unique_csv_name = f"{unique_csv_id}_{name}{ext}"
+    
+    #Open and save the uploaded csv file to trades_csv folder   
+    with open(f"pnl_calendar/trades_csv/{unique_csv_name}", "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
