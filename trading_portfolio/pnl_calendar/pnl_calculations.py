a7c47e67fd30d754e0e841cb436d7a0c27690af6
Kam1217 <kamilaostroga@gmail.com>
2025-07-04 12:15:22 +0100

    Add  all time pnl overview function

--- trading_portfolio/pnl_calendar/pnl_calculations.py
+++ trading_portfolio/pnl_calendar/pnl_calculations.py
@@ -3,7 +3,14 @@ from django.db.models.functions import TruncDate
 from .models import Trades
 
 #Function to calculate overview data
-
+def overview_pnl():
+    total_pnl = Trades.objects.aggregate(
+        total_gross_pnl = Sum("gross_pnl"),
+        total_fee = Sum("fee"),
+        total_net_pnl = Sum("net_pnl"),
+        number_of_trades = Count("trade_id"),
+    )
+    return (total_pnl)
 
 #Fucntion to calculate individual day data for the calendar
 def calendar_pnl():
@@ -15,3 +22,5 @@ def calendar_pnl():
         number_of_trades = Count("trade_id"),
     )
     return list(daily_pnl)
+
+#Function to calculate end of week pnl
