48a0de0dd480d32d27497d2d06cf6cb15e635270
Kam1217 <kamilaostroga@gmail.com>
2025-07-04 11:06:05 +0100

    Calculations.py file which will have all of the functions required to calculate the correct pnl data

---
+++ trading_portfolio/pnl_calendar/pnl_calculations.py
@@ -0,0 +1,7 @@
+from django.db.models import Sum
+from django.db.models.functions import TruncDate
+from .models import Trades
+
+#Function to calculate overview data
+
+#Fucntion to calculate individual day data for the calendar
