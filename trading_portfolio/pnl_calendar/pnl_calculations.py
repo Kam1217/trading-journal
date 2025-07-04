92707cd4f8dc44a3b09934700b72caa1f67e1d55
Kam1217 <kamilaostroga@gmail.com>
2025-07-04 11:39:43 +0100

    Calculate daily pnl

--- trading_portfolio/pnl_calendar/pnl_calculations.py
+++ trading_portfolio/pnl_calendar/pnl_calculations.py
@@ -4,4 +4,14 @@ from .models import Trades
 
 #Function to calculate overview data
 
+
 #Fucntion to calculate individual day data for the calendar
+def calendar_pnl():
+    daily_pnl = Trades.objects.annotate(
+        date = TruncDate("trade_date")
+    ).values("date").annotate(
+        total_gross_pnl = Sum("gross_pnl"),
+        total_fee = Sum("fee"),
+        total_net_pnl = Sum("net_pnl")
+    )
+    return list(daily_pnl)
