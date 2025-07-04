d21f613d8701adc439e100db550abc2e4a22baa5
Kam1217 <kamilaostroga@gmail.com>
2025-07-04 14:48:24 +0100

    Redirect to stop duplicate files uploading upon refresh

--- trading_portfolio/pnl_calendar/views.py
+++ trading_portfolio/pnl_calendar/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UploadForm
 from .handle_csv_upload import handle_upload_csv
 from .pnl_calculations import calendar_daily_pnl, overview_pnl, calendar_weekly_pnl
@@ -15,8 +15,9 @@ def pnl_calendar(request):
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid(): 
             handle_upload_csv(request.FILES["csv_file"])
+            return redirect("pnl_calendar")
     else:
         form = UploadForm()
     context["form"] = form
-    return render(request,"pnl_calendar.html", context)
+    return render(request,"pnl_calendar.html", context)  
 
