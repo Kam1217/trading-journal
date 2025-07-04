eefa4aff5c48840e9feda9f4636cf750795a3fab
Kam1217 <kamilaostroga@gmail.com>
2025-07-04 11:44:52 +0100

    Remove gross from calendar view and add numbe of trades instead

--- trading_portfolio/pnl_calendar/pnl_calculations.py
+++ trading_portfolio/pnl_calendar/pnl_calculations.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum
+from django.db.models import Sum, Count
 from django.db.models.functions import TruncDate
 from .models import Trades
 
@@ -10,8 +10,8 @@ def calendar_pnl():
     daily_pnl = Trades.objects.annotate(
         date = TruncDate("trade_date")
     ).values("date").annotate(
-        total_gross_pnl = Sum("gross_pnl"),
         total_fee = Sum("fee"),
-        total_net_pnl = Sum("net_pnl")
+        total_net_pnl = Sum("net_pnl"),
+        number_of_trades = Count("trade_id"),
     )
     return list(daily_pnl)
