0596d49b6571a6df5eae60d6c822d052a999fd49
Kam1217 <kamilaostroga@gmail.com>
2025-07-07 13:54:26 +0100

    clean up process_csv_to_trades

--- trading_portfolio/pnl_calendar/handle_csv_upload.py
+++ trading_portfolio/pnl_calendar/handle_csv_upload.py
@@ -12,13 +12,13 @@ def save_uploaded_csv(f):
     file_path = f"pnl_calendar/trades_csv/{unique_csv_name}"
 
     #Open and save the uploaded csv file to trades_csv folder   
-    with open(f"pnl_calendar/trades_csv/{unique_csv_name}", "wb+") as destination:
+    with open(file_path, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
     
     return file_path
 
-def process_csv_to_trades(f):
+def process_csv_to_trades(csv_file_path):
     #Get required csv data for the Trade model
     desired_keys = {"Date/Time": "trade_date", "Gross P/L": "gross_pnl", "Fee": "fee", "Net P/L": "net_pnl", "Trade ID": "trade_id"}
 
@@ -26,7 +26,7 @@ def process_csv_to_trades(f):
     new_trade_obj_count = 0
     updated_trade_obj_count = 0
 
-    with open(file_path, mode= "r") as file:
+    with open(csv_file_path, mode= "r") as file:
         csv_file = csv.DictReader(file)
     
     #Raise error if required keys are missing in the CSV file 
@@ -39,8 +39,7 @@ def process_csv_to_trades(f):
     for row in csv_file:
         filtered_row = {desired_keys[key]: row[key] for key in desired_keys.keys() if key in row} 
 
-    #Convert date to correct model format for Trade model
-
+        #Convert date to correct model format for Trade model
         date_obj = datetime.strptime(filtered_row["trade_date"], "%d/%m/%Y %H:%M:%S %z")
         filtered_row["trade_date"] = date_obj.strftime("%Y-%m-%d %H:%M:%S%z")
 
