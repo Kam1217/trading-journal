3d167ef9930e9e376ae953afe8907ba068b4ee4f
Kam1217 <kamilaostroga@gmail.com>
2025-07-11 10:40:41 +0100

    update view.py without try block and with calendar function

--- trading_portfolio/pnl_calendar/views.py
+++ trading_portfolio/pnl_calendar/views.py
@@ -1,43 +1,39 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from datetime import datetime, date
-import calendar
+from datetime import date
 from .forms import UploadForm
 from .handle_csv_upload import handle_upload_csv
 from .pnl_calculations import calendar_daily_pnl, overview_pnl, calendar_weekly_pnl
+from .generate_calendar import generate_calendar
 
 # Create your views here.
 
 def pnl_calendar(request):
-    context = {
-        "overview_pnl": overview_pnl(),
-        "calendar_daily_pnl": calendar_daily_pnl(),
-        "calendar_weekly_pnl" : calendar_weekly_pnl(),
-        }
     if request.POST:
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            try:
-                csv_files = form.cleaned_data["csv_file"]
-                processed_count = 0
-
-                for csv_file in csv_files: 
-                    handle_upload_csv(csv_file)
-                    processed_count += 1
-
-                messages.success(request,f"Successfully processed {processed_count} CSV file(s)!")
-            
-            except Exception as e:
-                messages.error(request, F"Error processing CSV: {str(e)}")
-            except Exception as e:
-                messages.error(request, f"An error occurred: {str(e)}")
+            handle_upload_csv(request.FILES["csv_file"])
             return redirect("pnl_calendar")
-        else:
-            for field,errors in form.errors.items():
-                for error in errors:
-                    messages.error(request,error)
     else:
         form = UploadForm()
-    context["form"] = form
-    return render(request,"pnl_calendar.html", context)  
 
+    today = date.today()
+    year = today.year
+    month = today.month
+    
+    calendar_weeks = generate_calendar(
+        calendar_daily_pnl(), 
+        calendar_weekly_pnl(), 
+        year, 
+        month
+    )
+    
+    context = {
+        "form" : form,
+        "overview_pnl": overview_pnl(),
+        "calendar_daily_pnl": calendar_daily_pnl(),
+        "calendar_weekly_pnl" : calendar_weekly_pnl(),
+        "calendar_weeks": calendar_weeks,
+        "current_month": today.strftime('%B %Y'),
+    }
+
+    return render(request,"pnl_calendar.html", context)  
